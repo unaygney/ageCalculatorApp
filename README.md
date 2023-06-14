2a339a013ed00c3a5c77ff29882909510a4cc686
guneyunay <mesafe18@hotmail.com>
2023-06-14 20:58:59 +0300

    age calculator

---
+++ README.md
@@ -0,0 +1,59 @@
+
+## Overview
+
+### Age Calculator App
+
+Users should be able to:
+
+- View an age in years, months, and days after submitting a valid date through the form
+- Receive validation errors if:
+  - Any field is empty when the form is submitted
+  - The day number is not between 1-31
+  - The month number is not between 1-12
+  - The year is in the future
+  - The date is invalid e.g. 31/04/1991 (there are 30 days in April)
+- View the optimal layout for the interface depending on their device's screen size
+- See hover and focus states for all interactive elements on the page
+- **Bonus**: See the age numbers animate to their final number when the form is submitted
+
+### Screenshot
+
+![](./screenshots/desktop.png.jpg)
+
+
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+
+### What I learned
+
+- Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+
+- I had a hard time writing the javascript part of this code. I took help from Stack Overflow and chatGBT. I saw my missing parts. It was a good app for my development. I will study more javascript
+
+
+
+
+## Author
+
+
+- Frontend Mentor - [@unaygney](https://www.frontendmentor.io/profile/unaygney)
+- Twitter - [@guneyunay](https://www.twitter.com/guneyunay)
+
+
+
---
+++ app.js
@@ -0,0 +1,127 @@
+function calculate() {
+    // Inputs
+    let dayInp = parseInt(document.getElementById('day').value);
+    let monthInp = parseInt(document.getElementById('month').value);
+    let yearInp = parseInt(document.getElementById('year').value);
+
+    // Outputs
+    let dayOtp = document.getElementById('DD');
+    let monthOtp = document.getElementById('MM');
+    let yearOtp = document.getElementById('YY');
+
+//     // Validation
+
+//      // Day Validation
+//   if (isNaN(dayInp) || dayInp < 1 || dayInp > 31) {
+//     document.getElementById('day-label').classList.add('error');
+//     document.getElementById('day').classList.add('error-line');
+//     document.getElementById('day-error').style.display = 'inline-block';
+//     return;
+//   } else {
+//     document.getElementById('day-label').classList.remove('error');
+//     document.getElementById('day').classList.remove('error-line');
+//     document.getElementById('day-error').style.display = 'none';
+//   }
+
+//   // Month validation
+//   if (isNaN(monthInp) || monthInp < 1 || monthInp > 12) {
+//     document.getElementById('month-label').classList.add('error');
+//     document.getElementById('month').classList.add('error-line');
+//     document.getElementById('month-error').style.display = 'inline-block';
+//     return;
+//   } else {
+//     document.getElementById('month-label').classList.remove('error');
+//     document.getElementById('month').classList.remove('error-line');
+//     document.getElementById('month-error').style.display = 'none';
+//   }
+
+//   // Year validation
+//   if (isNaN(yearInp)) {
+//     document.getElementById('year-label').classList.add('error');
+//     document.getElementById('year').classList.add('error-line');
+//     document.getElementById('year-error').style.display = 'inline-block';
+//     return;
+//   } else {
+//     document.getElementById('year-label').classList.remove('error');
+//     document.getElementById('year').classList.remove('error-line');
+//     document.getElementById('year-error').style.display = 'none';
+//   }
+
+
+  // Validation
+  let errors = []; // Hataları tutmak için dizi
+
+  // Day Validation
+  if (isNaN(dayInp) || dayInp < 1 || dayInp > 31) {
+    errors.push('day');
+  }
+
+  // Month validation
+  if (isNaN(monthInp) || monthInp < 1 || monthInp > 12) {
+    errors.push('month');
+  }
+
+  // Year validation
+  if (isNaN(yearInp)) {
+    errors.push('year');
+  }
+
+  // Display Errors
+  for (let i = 0; i < errors.length; i++) {
+    document.getElementById(errors[i] + '-label').classList.add('error');
+    document.getElementById(errors[i]).classList.add('error-line');
+    document.getElementById(errors[i] + '-error').style.visibility = 'visible';
+  }
+
+  // Clear Errors
+  if (errors.length === 0) {
+    document.getElementById('day-label').classList.remove('error');
+    document.getElementById('day').classList.remove('error-line');
+    document.getElementById('day-error').style.display = 'none';
+    document.getElementById('month-label').classList.remove('error');
+    document.getElementById('month').classList.remove('error-line');
+    document.getElementById('month-error').style.display = 'none';
+    document.getElementById('year-label').classList.remove('error');
+    document.getElementById('year').classList.remove('error-line');
+    document.getElementById('year-error').style.display = 'none';
+  } else {
+    return; // Hatalı girdiler olduğunda işlemi durdur
+  }
+
+
+    // Today Date
+    let todayDate = new Date();
+    let currentDate = todayDate.getDate();
+    let currentMonth = todayDate.getMonth() + 1;
+    let currentYear = todayDate.getFullYear();
+
+    // Birth Date
+    let birthDate = new Date(yearInp, monthInp - 1, dayInp);
+    let birthDay = birthDate.getDate();
+    let birthMonth = birthDate.getMonth() + 1;
+    let birthYear = birthDate.getFullYear();
+
+    // Age Calculation
+    let ageInMilliseconds = todayDate - birthDate;
+    let ageInDays = Math.floor(ageInMilliseconds / (1000 * 60 * 60 * 24));
+    let ageInYears = currentYear - birthYear;
+    let ageInMonths = currentMonth - birthMonth;
+
+    if (currentMonth < birthMonth || (currentMonth === birthMonth && currentDate < birthDay)) {
+        ageInYears--;
+        ageInMonths += 12;
+    }
+
+    if (currentDate < birthDay) {
+        ageInMonths--;
+        let lastMonthDate = new Date(currentYear, currentMonth - 1, 0).getDate();
+        ageInDays = lastMonthDate - birthDay + currentDate;
+    } else {
+        ageInDays = currentDate - birthDay;
+    }
+
+    // Display Results
+    dayOtp.textContent = ageInDays;
+    monthOtp.textContent = ageInMonths;
+    yearOtp.textContent = ageInYears;
+}
